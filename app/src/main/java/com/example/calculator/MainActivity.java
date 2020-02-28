ac3e62a237b06c027726792312ad17967a141d7e
Absolutely <absolutely1756@gmail.com>
2020-02-28 23:52:43 +0700

    Listen down Enter Refactoring

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editExpression);
+        editText.setOnKeyListener(this);
     }
-
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                (keyCode == KeyEvent.KEYCODE_ENTER))
+        {
+            calculateExpression(view);
+            return true;
+        }
+        return false;
+    }
     public void calculateExpression(View view) {
         EditText editText = findViewById(R.id.editExpression);
         TextView showText = findViewById(R.id.showText);
@@ -31,12 +44,26 @@ public class MainActivity extends AppCompatActivity {
         //Преобразовываем из инфиксной в ОПЗ
         reversePN.TransformToRPN(expression);
         //Проверка полученного выражения в ОПЗ на ошибки
-        if (!validator.CheckRPNOnErrors(reversePN.getResult(), reversePN.getCountNumbers(), reversePN.getCountOperations())) {
-            editText.setError("Строка имеет ошибки. Исправьте их и повторите.");
-            return;
+        switch (validator.CheckRPNOnErrors(reversePN.getResult(), reversePN.getCountNumbers(), reversePN.getCountOperations())) {
+            case 1: {
+                editText.setError("Скобки расставлены неверно.");
+                return;
+            }
+            case 2: {
+                editText.setError("Количество чисел не соответствует количеству операций.");
+                return;
+            }
         }
         //Подсчет выражения
-        String responseOnExpression = new DecimalFormat("#.##").format(reversePN.CalculateRPN());
+        String responseOnExpression;
+        double resultExpression = 0;
+        try {
+            resultExpression = reversePN.CalculateRPN();
+        }
+        catch(Exception e){
+            editText.setError("В введенной строке ошибка. Исправьте её и повторите.");
+        }
+        responseOnExpression = new DecimalFormat("#.##").format(resultExpression);
         showText.setText(responseOnExpression);
     }
 }
